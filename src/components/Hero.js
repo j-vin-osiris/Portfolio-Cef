3b17b727161644ca3109bd137242dc0785dbc613
jvin <jvboetto@gmail.com>
2025-01-07 10:45:00 +0100

    creation de la modale + intégration sur page HOME

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,5 @@
 import myImage from "../assets/images/hero-bg.jpg";
+import Modal from "./modale";
 
 // construction de la section "Hero" avec photo+text + import du component modal
 const Hero = () => {
@@ -16,6 +17,8 @@ const Hero = () => {
       <div className="text-center">
         <h1 className="display-1">Bonjour, Je suis John Doe</h1>
         <h2 className="display-4">Développeur web full stack</h2>
+
+        <Modal />
       </div>
     </section>
   );
